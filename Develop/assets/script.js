7746389580c14f93c36db978ea4c2b99efcfa385
Gintstir <gint.stirbys@gmail.com>
2020-10-31 19:14:59 -0400

    changed file tree structure, created readme,md

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -8,53 +8,61 @@ var promptLength = function () {
 
 var promptChars = function () {
   var options = [false, false, false, false];
-  lowerCase = window.confirm("Use lowercase characters? (a-z):");
-  upperCase = window.confirm("Use uppercase characters? (A-Z):");
-  numbers = window.confirm("Use numeric characters? (0-9):");
-  specChars = window.confirm("Use special characters? ('!', '[', '=', etc.):");
+  lowerCase = window.confirm("Do you wish to include lower case letters (a-z)?");
+  upperCase = window.confirm("Do you wish to include UPPERCASE letters (A-Z)?");
+  numbers = window.confirm("Do you wish to include numbers (0-9)?");
+  specChars = window.confirm("Do you wish to include special characters (!@#$%^& etc.)?");
   return options;
 }
 
 
-var generatePassword = function () {
-  // Get password length
+var createPW = function () {
+  
   var passLength = 0;
-  passLength = promptLength(); // Ask first time
-  while ((passLength < 8 || passLength > 128) || !parseInt(passLength)) // Verify user enters valid option, if not, prompt until they do.
+  passLength = promptLength();
+  // check that user has entered valid password length, if not, prompt until they do.  
+  while ((passLength < 8 || passLength > 128) || !parseInt(passLength)) 
   {
-    window.alert("Invalid Length (8-128 req.)"); // Notify of problem
-    passLength = promptLength(); // Ask again
+    window.alert("Invalid Length! A value between 8 and 128 is required! Please try again."); // Notify of problem
+    passLength = promptLength(); // prompt again
   }
 
   // Ask for character options, indecies: 0 = lowercase, 1 = uppercase, 2 = numeric, 3 = special
   var options = promptChars();
-  while (!(lowerCase || upperCase || numbers || specChars)) // Verify user selects at least one option, if not, prompt until they do.
-  {
-    window.alert("You must select 'Ok' for at least one option."); // Notify of problem
-    // Ask again
-    options = promptChars();
-  }
+    while (!(lowerCase || upperCase || numbers || specChars)) // Verify user selects at least one option, if not, prompt until they do.
+    {
+      window.alert("You have not selected any character sets, please try again and choose at least one option.  Thank you!"); // Notify of problem
+      // prompt again
+      options = promptChars();
+    }
 
   // ACCEPTABLE CHARACTER FOR PASSWORD: a-z, A_Z, 0-9,  !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 //Create usable array to reference characters
   //using method String.fromCharCode to define available characters
+  //string.fromCharCode:  97-122 = abcdefghijklmnopqrstuvwxyz
   var useableChars = [];
-  if (lowerCase) // Add lowercase chars if selected
+  if (lowerCase) 
   {
     for (var i = 97; i <= 122; i++)
       useableChars.push(String.fromCharCode(i));
   }
+  //string.fromCharCode: 65-90 = ABCDEFGHIJKLMNOPQRSTUVWXYZ
   if (upperCase) // Add uppercase chars if selected
   {
     for (var i = 65; i <= 90; i++)
       useableChars.push(String.fromCharCode(i));
   }
+  //string.fromCharCode: 48-57 = 0123456789
   if (numbers) // Add numeric chars if selected
   {
     for (var i = 48; i <= 57; i++)
       useableChars.push(String.fromCharCode(i));
   }
-  if (specChars) // Add special chars if selected
+  //string.fromCharCode: 32-47 =  !"#$%&'()*+,-./
+  //   "       "       : 58-64 = :;<=>?@
+  //   "       "       : 91-96 = [\]^_`
+  //   "       "       : 123-126 = {|}~
+  if (specChars) 
   {
     for (var i = 32; i <= 47; i++)
       useableChars.push(String.fromCharCode(i));
@@ -63,31 +71,22 @@ var generatePassword = function () {
     for (var i = 91; i <= 96; i++)
       useableChars.push(String.fromCharCode(i));
     for (var i = 123; i <= 126; i++)
-      useableChars.push(String.fromCharCode(i));
+      useableChars.push(String.fromCharCode(i));    
   }
 
   
 
-  // Generate password
-  // Initialize empty password
+  // Generate password and account for null/""  password
   var password = "";
-  // for loop that executes passLength num of times 
+  // create 'for' loop that executes passLength num of times 
   for (var i = 0; i < passLength; i++)
-  // Add random character from useableChars to password
+  // Add selected useableChars to password use math.random
     password += useableChars[Math.ceil(Math.random() * useableChars.length) - 1]; 
   return password;
 }
 
-
-
-
-
-
-  
-
-
-
-
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 // Get references to the #generate element
@@ -95,7 +94,7 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = createPW();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# Password Generator Starter Code
+#  **Random Password Generator**
+
+This is the completed third weekly challenge for the UPENN LPS Coding Bootcamp.
+The project required us to take an existing index.html with a correspong style.css and create a script.js that would run the asked for program.  Namely, the JS(Javascript) code we were asked to write, required us to prompt the user about a number of criteria:
+**(required)**
+    1. How long the user wished the pw(password) to be.
+        - the pw must be between 8 and 128 chracters long.
+**(Optional- but the user must pick at least one of the following option)**
+    2. If the user wished to include lowercase letters (a-z).
+    3. If the user wished to include uppercase letters (A-Z).
+    4. If the user wished to include numers (0-9).
+    5. If the user wished to include special characters (!,@,#,$,%,^,",&,*, etc.)
+The preceding criteria are then used to generate a random password. 
+
+## **Link**
+
+Here is a link to the [deployed application](https://gintstir.github.io/password-generator/) 
+
+Here is a link to the [github repository](https://github.com/Gintstir/password-generator)
+
+## **Motivation**
+
+This was a very difficult challenge.  It helped reinforce some very- for me at least- hard-to-grasp concepts.  These included:
+- learning introductory JS.
+- Gaining a better understanding of JS syntax and structure
+- Learning about and implementing:
+    - object methods
+    - Functions
+    - User input
+    - Loop syntax
+    - The list goes on and on.
+
+  
+
+## **Build Status**
+
+For the pruposes of this weekly challenge this build should be considered complete barring any mistakes or omissions by me.  
+
+## **Code Style**
+
+This project came preloaded with basic HTML and CSS.  We were required to write Javascript to complete all the project criteria.  
+
+## **Screenshots**  
+This is a screen shot of the deployed website:
+>![Deployed-website](Develop\assets\images\before.png)
+
+This is a screen shot of the deployed website showing a created password:
+>![Deployed-website-with-password](Develop\assets\images\before.png)
+
+
+## **Tech Used**
+
+- Built With
+    - CSS
+    - HTML
+    - Javascript
+    - [VS Code](https://code.visualstudio.com/)
+    - [Lighthouse](https://developers.google.com/web/tools/lighthouse/)
+    - Lots of Youtube videos
+    - And of course MDN
+    
+    
+
+## **Credits**
+
+Thanks to UPennLPS Coding Bootcamp.  Thanks to the original creator of the [repository](https://github.com/coding-boot-camp/friendly-parakeet)  used in this assignment: [Xandromus](https://github.com/Xandromus).  Thank you to the bootcamp instructors and TA's for all their help along the way ❤.  
+
